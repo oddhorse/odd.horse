9efd7c67f4abbdecd6577a070b5873d6bbb3ec06
bear <bear@odd.horse>
2022-10-10 19:37:16 -0400

    updated to use prettier and jshint

--- js/scatter.js
+++ js/scatter.js
@@ -2,63 +2,73 @@
  * scatter.js copyright (c) 2022 oddhorse all rights reserved uwu
  */
 
-let timer;
-let lastTime;
-let ticker = 0;
-let limit = Math.random() * 1500 + 500;
-let chaosScale = 0.1;
+let timer
+let lastTime
+let ticker = 0
+let limit = Math.random() * 1500 + 500
+let chaosScale = 0.1
+
+const CHARS = {
+  o: "oOºÒÓÔÕÖòóôõöŌōŎŏŐőƠơǑǒǪǫǬǭȌȍȎȏȪȫȬȭȮȯȰȱᴼᵒṌṍṎṏṐṑṒṓỌọỎỏỐốỒồỔổỖỗỘộỚ🄞⒪Ⓞⓞ🄾🅞🅾Ｏｏ𝐎𝐨𝑂𝑜𝑶𝒐𝒪𝓞𝓸𝔒𝔬𝕆𝕠𝕺𝖔𝖮𝗈𝗢𝗼𝘖𝘰𝙊𝙤𝙾𝚘".split(
+    ""
+  ),
+}
 
 function playAnimation(time) {
-  if (lastTime != null) {
-    const delta = time - lastTime;
-    ticker += delta;
+  if (lastTime !== null) {
+    const delta = time - lastTime
+    ticker += delta
 
     if (ticker >= limit) {
-      ticker = 0;
-      limit = Math.random() * 1500 + 500;
-      scatter ("oddhorse".split(""), "oddhorse", elLogoText);
+      ticker = 0
+      limit = Math.random() * 1500 + 500
+      scatter("oddhorse".split(""), "oddhorse", elLogoText)
     }
   }
 
-  lastTime = time;
-  window.requestAnimationFrame(playAnimation);
+  lastTime = time
+  window.requestAnimationFrame(playAnimation)
 }
 
-const elLogoText = document.getElementById("logo-text");
+const elLogoText = document.getElementById("logo-text")
 
-function randOld (scale) {
-  let num = Math.random();
-  return scale * 2 * num - scale;
+function randOld(scale) {
+  let num = Math.random()
+  return scale * 2 * num - scale
 }
 
-function rand (scale) {
-  let num = Math.random();
-  let scaleFlavor = Math.random() - 0.5;
-  return scale * 2 * num - scale + scaleFlavor;
+function rand(scale) {
+  let num = Math.random()
+  let scaleFlavor = Math.random() - 0.5
+  return scale * 2 * num - scale + scaleFlavor
 }
 
-function scatter (chars, text, el) {
-  let spanned = "";
+function scatter(chars, text, el) {
+  let spanned = ""
   for (let i = 0; i < chars.length; i++) {
-    spanned += `<span style="position:relative;top:${rand(chaosScale)}em;left:${rand(chaosScale * 0.5)}em;">${chars[i]}</span>`
+    if (chars[i] === "o")
+      chars[i] = CHARS.o[Math.floor(Math.random() * CHARS.o.length)]
+    spanned += `<span style="position:relative;top:${rand(
+      chaosScale
+    )}em;left:${rand(chaosScale * 0.5)}em;">${chars[i]}</span>`
   }
-  el.innerHTML = spanned;
+  el.innerHTML = spanned
 }
 
-function agitate (text, hoverBox, el) {
-  hoverBox.style.padding = "2em";
-  hoverBox.style.margin = "-2em";
-  const chars = text.split("");
-  chaosScale = 1;
-  ticker = 0;
-  scatter(chars, text, el);
+function agitate(text, hoverBox, el) {
+  hoverBox.style.padding = "2em"
+  hoverBox.style.margin = "-2em"
+  const chars = text.split("")
+  chaosScale = 1
+  ticker = 0
+  scatter(chars, text, el)
   // timer = setInterval(scatter, 350, chars, text, el)
 }
 
-function returnTo (text, hoverBox, el) {
+function returnTo(text, hoverBox, el) {
   // clearInterval(timer);
-  chaosScale = 0.1;
-  hoverBox.style.padding = "0";
-  hoverBox.style.margin = "0";
+  chaosScale = 0.1
+  hoverBox.style.padding = "0"
+  hoverBox.style.margin = "0"
 }
-window.requestAnimationFrame(playAnimation);
+window.requestAnimationFrame(playAnimation)
