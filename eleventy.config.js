4078620590b968274815d74497ad418eb13bf721
oddhorse <15389659+oddhorse@users.noreply.github.com>
2025-01-13 20:24:49 -0500

    added 11ty HtmlBasePlugin (?) because ig ineed that um i guess to rebase the fucking uhh url thing uhh uhhhhhhhhhh

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ import markdownIt from 'markdown-it'
 import markdownItAnchor from 'markdown-it-anchor'
 import { DateTime } from 'luxon'
 
+import { HtmlBasePlugin } from '@11ty/eleventy'
+
 import EleventyPluginNavigation from '@11ty/eleventy-navigation'
 import EleventyPluginBundle from '@11ty/eleventy-plugin-bundle'
 import EleventyPluginSyntaxhighlight from '@11ty/eleventy-plugin-syntaxhighlight'
@@ -21,6 +23,7 @@ export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(EleventyPluginSyntaxhighlight)
 	eleventyConfig.addPlugin(EleventyPluginBundle)
 	eleventyConfig.addPlugin(pluginFilters)
+	eleventyConfig.addPlugin(HtmlBasePlugin)
 	eleventyConfig.addPlugin(EleventyVitePlugin, {
 		tempFolderName: '.11ty-vite', // Default name of the temp folder
 
