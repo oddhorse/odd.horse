1094f4b90781b086602f0a20edd55a5c3c380bd2
oddhorse <15389659+oddhorse@users.noreply.github.com>
2025-01-16 18:09:30 -0500

    FLESHING OUT MUSIC PAGE

--- eleventy.config.js
+++ eleventy.config.js
@@ -138,6 +138,10 @@ export default async function (eleventyConfig) {
 		}
 	})
 
+	eleventyConfig.addFilter('lyrics', (data) => {
+		return 'hi!' + data
+	})
+
 	// Copy/pass-through files
 	eleventyConfig.addPassthroughCopy('src/assets/css')
 	eleventyConfig.addPassthroughCopy('src/assets/js')
